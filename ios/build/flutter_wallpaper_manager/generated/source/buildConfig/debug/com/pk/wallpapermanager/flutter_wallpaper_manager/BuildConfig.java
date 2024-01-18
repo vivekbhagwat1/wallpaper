8dd6055e202ffe1f2a2b236e85f71667d4c8a32b
vivekbhagwat1 <bhagwatvivek4@gmail.com>
2024-01-18 19:54:47 +0530

    Wallpaper

---
+++ ios/build/flutter_wallpaper_manager/generated/source/buildConfig/debug/com/pk/wallpapermanager/flutter_wallpaper_manager/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pk.wallpapermanager.flutter_wallpaper_manager;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.pk.wallpapermanager.flutter_wallpaper_manager";
+  public static final String BUILD_TYPE = "debug";
+}
